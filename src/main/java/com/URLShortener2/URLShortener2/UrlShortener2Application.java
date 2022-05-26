7d0734aa4ff22b8580a3f8a33915a720791489a1
Venkatesh A <venkatesh.atchutha09@gmail.com>
2022-05-26 18:59:19 +0530

    test

---
+++ src/main/java/com/URLShortener2/URLShortener2/UrlShortener2Application.java
@@ -0,0 +1,13 @@
+package com.URLShortener2.URLShortener2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortener2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortener2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/URLShortener2/URLShortener2/UrlShortener2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.URLShortener2.URLShortener2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortener2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
